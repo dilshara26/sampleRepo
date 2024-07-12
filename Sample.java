754d60dc8d379105b3371dea5c8243f9756bfd02
dilshara26 <dilsharahasanka@gmail.com>
2024-07-12 21:51:30 +0530

    products added newly

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample {
 
     public static void main(String[] args) {
         // add a sout here
-//        Product cards are added
+//        Product cards are added newly
 
         // another sout
         System.out.println("Hello ruwan");
